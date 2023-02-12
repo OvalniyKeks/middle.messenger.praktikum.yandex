81a515a7d5233a5de48b11b3315ae67f9d5c4a90
OvalniyKeks <keksovalniy@gmail.com>
2023-02-12 18:30:07 +0300

    add page auth, regisration, chats

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { pages } from "./pages.js";
-
-import './partials.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-  const app = document.querySelector('#app');
-
-  app.innerHTML = pages.auth();
-});
